dd45e6567830bca33037f63851d8074ca00af065
Gianluca Truda <dev@gianluca.ai>
2025-03-21 20:37:26 +0000

    Don't auto open browser

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   root: resolve(__dirname, 'src'), // assumes your client code is under /src
   server: {
-    open: true // automatically open the browser
+    open: false
   },
   build: {
     outDir: resolve(__dirname, 'dist')
